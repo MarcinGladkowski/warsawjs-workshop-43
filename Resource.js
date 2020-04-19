a685084c85a84e8d6d74b90630ae78997331cb87
Martin89PL <marcingladkowski@gmail.com>
2020-04-19 12:05:49 +0200

    Implementation of Resource

--- Resource.js
+++ Resource.js
@@ -2,4 +2,28 @@ import Model from './Model.js'
 
 export default class Resource extends Model {
 
+    startLoading() {
+        this.update({loading: true, data: null, errors: null})
+    }
+
+    isLoading() {
+        return this.get('loading');
+    }
+
+    stopLoading(data) {
+        this.update({loading: false, data: data})
+    }
+
+    getData() {
+        return this.get('data');
+    }
+
+    handleError(error) {
+        return this.set('error', error);
+    }
+
+    getError() {
+        return this.get('error');
+    }
+
 }
