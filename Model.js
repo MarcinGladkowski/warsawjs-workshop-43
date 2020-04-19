e2be56534fa7ad9ff34f72ebf31b1106874ee4d6
Martin89PL <marcingladkowski@gmail.com>
2020-04-19 11:00:11 +0200

    simple implementation of update method

--- Model.js
+++ Model.js
@@ -7,14 +7,23 @@ export default class Model {
     get(key) {
 
         if (!this.has(key)) {
-            throw new Error(`No field in model for key: ${key}`)
+            throw new Error(`No field in model for key: ${key}`);
         }
 
         return this[key];
     }
+    
+    update(obj) {
+        const keys = Object.keys(obj);
+        const values = Object.values(obj);
+
+        for(let i = 0; i < keys.length; i++) {
+            this[keys[i]] = values[i]
+        }
+    }
 
     has(key) {
-        return (this[key] !== undefined)
+        return (this[key] !== undefined);
     }
 
 }
