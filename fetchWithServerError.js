4212484140ec1103af605c8b046137f3d9551086
Martin89PL <marcingladkowski@gmail.com>
2020-04-19 11:32:47 +0200

    throw error when status code  !== 200

--- fetchWithServerError.js
+++ fetchWithServerError.js
@@ -1,3 +1,11 @@
-export default function fetchWithServerError() {
-
+export default function fetchWithServerError(url) {
+    fetch(url)
+    .then((response) => {
+        if (response.status !== 200) {
+            throw new Error(`Invalid response status code ${response.status}`)
+        }
+    })
+    .then((response) => {
+        return response.json();
+    });
 }
