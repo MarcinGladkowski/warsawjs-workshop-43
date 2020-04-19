ae9fa978298786960db11aceb586592423ef62ec
Martin89PL <marcingladkowski@gmail.com>
2020-04-19 12:41:38 +0200

    start implementation ResourceFactory

--- ResourceFactory.js
+++ ResourceFactory.js
@@ -1,3 +1,16 @@
+import Resource from "./Resource.js";
+
 export default function ResourceFactory() {
+
+    let loading = false;
+
+    return {
+        startLoading() {
+            loading = true;
+        },
+        isLoading() {
+            return loading;
+        }
+    }
 
 }
